934b93cd3b219c27e0eedc7745cbe0479fdb8da1
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-02-11 07:47:17 +0600

    utility config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-// require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("MongoDB connection Failed !! ", err));
