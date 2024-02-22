b34a1f43a3a4b4d731386905b66de92983461d47
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-02-22 09:24:00 +0600

    add comment logic

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,18 +8,78 @@ const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+
+  const comments = await Comment.find({ video: videoId })
+    .sort({ createdAt: -1 })
+    .skip((page - 1) * limit)
+    .limit(limit);
+
+  const totalComments = await Comment.countDocuments({ video: videoId });
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        comments,
+        totalComments,
+        totalPages: Math.ceil(totalComments / limit),
+        currentPage: page,
+      },
+      "Comments fetched successfully"
+    )
+  );
 });
 
 const addComment = asyncHandler(async (req, res) => {
   // TODO: add a comment to a video
+  const { videoId } = req.params;
+  const { content } = req.body;
+
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user._id,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, comment, "Comment added successfully"));
 });
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new ApiError(404, "Comment not found");
+  }
+
+  comment.content = content;
+  await comment.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comment updated successfully"));
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
+
+  const { commentId } = req.params;
+
+  const comment = await Comment.findByIdAndDelete(commentId);
+
+  if (!comment) {
+    throw new ApiError(404, "Comment not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Comment deleted successfully"));
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -2,16 +2,109 @@ import mongoose from "mongoose";
 import { Video } from "../models/video.model.js";
 import { Subscription } from "../models/subscription.model.js";
 import { Like } from "../models/like.model.js";
+import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  // TODO: Get the channel stats of total video views, total subscribers, total videos, total likes etc.
+   try {
+    // Get the user's channel ID from the request
+    const { channelId } = req.params;
+
+    // Find the user by their channel ID
+    const user = await User.findById(channelId);
+
+    if (!user) {
+      throw new ApiError(404, "User not found");
+    }
+
+    // Get the total video views
+    const totalVideoViews = await Video.aggregate([
+      {
+        $match: {
+          owner: user._id,
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalViews: { $sum: "$views" },
+        },
+      },
+    ]);
+
+    // Get the total subscribers
+    const totalSubscribers = await User.aggregate([
+      {
+        $match: {
+          subscriptions: user._id,
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalSubscribers: { $sum: 1 },
+        },
+      },
+    ]);
+
+    // Get the total videos
+    const totalVideos = await Video.countDocuments({ owner: user._id });
+
+    // Get the total likes
+    const totalLikes = await Video.aggregate([
+      {
+        $match: {
+          owner: user._id,
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalLikes: { $sum: "$likes" },
+        },
+      },
+    ]);
+
+    // Return the channel stats
+    return res.status(200).json(
+      new ApiResponse(200, {
+        totalVideoViews: totalVideoViews[0]?.totalViews || 0,
+        totalSubscribers: totalSubscribers[0]?.totalSubscribers || 0,
+        totalVideos,
+        totalLikes: totalLikes[0]?.totalLikes || 0,
+      }, "Channel stats fetched successfully")
+    );
+  } catch (error) {
+    throw new ApiError(500, "Failed to fetch channel stats");
+  }
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
   // TODO: Get all the videos uploaded by the channel
+  try {
+    // Get the user's channel ID from the request
+    const { channelId } = req.params;
+
+    // Find the user by their channel ID
+    const user = await User.findById(channelId);
+
+    if (!user) {
+      throw new ApiError(404, "User not found");
+    }
+
+    // Get all the videos uploaded by the channel
+    const videos = await Video.find({ owner: user._id });
+
+    // Return the channel videos
+    return res.status(200).json(
+      new ApiResponse(200, videos, "Channel videos fetched successfully")
+    );
+  } catch (error) {
+    throw new ApiError(500, "Failed to fetch channel videos");
+  }
 });
 
 export { getChannelStats, getChannelVideos };
